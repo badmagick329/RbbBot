b308fd90ea09009ecd2bde4ac82434c26ab7708d
badmagick <uzairfarooqi@gmail.com>
2023-06-15 18:22:02 +0100

    fix: disabled twitter

--- rbb_bot/cogs/sns_cog.py
+++ rbb_bot/cogs/sns_cog.py
@@ -43,7 +43,7 @@ class SnsCog(Cog):
             logger=self.bot.logger,
         )
         self.sns_dict = {
-            "twitter": Sns(twitter_fetcher),
+            # "twitter": Sns(twitter_fetcher),
             # "instagram": Sns(instagram_fetcher, timestamped_urls=True),
             "tiktok": Sns(tiktok_fetcher),
             "reddit": Sns(reddit_fetcher, cache_files=False),
@@ -57,7 +57,7 @@ class SnsCog(Cog):
         self.instagram_fetcher.web_client.close()
 
     @commands.hybrid_command(
-        name="sns", brief="Get posts from twitter, tiktok or reddit"
+        name="sns", brief="Get posts from tiktok or reddit"
     )
     @commands.bot_has_permissions(send_messages=True, embed_links=True)
     @commands.cooldown(2, 5, commands.BucketType.user)
@@ -66,7 +66,7 @@ class SnsCog(Cog):
         self, ctx: Context, with_text: Optional[bool] = False, *, urls: str
     ):
         """
-        Get posts from twitter, tiktok or reddit
+        Get posts from tiktok or reddit
 
         Not supported: tiktok photos, reddit videos with sound
 
